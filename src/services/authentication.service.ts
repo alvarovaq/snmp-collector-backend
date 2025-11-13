38982e3b1a39f87e9a59d6a84868932d9f732b05
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2025-11-13 18:19:23 +0100

    Creamos servicio para la autenticacion y creamos funciones de ayuda

---
+++ src/services/authentication.service.ts
@@ -0,0 +1,29 @@
+import generator from "generate-password";
+import bcrypt from "bcrypt";
+import { logger } from "./logger.service";
+
+export class AuthenticationService {
+    private makeRandomPassword (): string {
+        return generator.generate({
+            length: 8,
+            numbers: true,
+            symbols: false,
+            uppercase: true,
+            excludeSimilarCharacters: true,
+        });
+    }
+
+    private async makeHash(password: string): Promise<string> {
+        const saltRounds = 10;
+        return await bcrypt.hash(password, saltRounds);
+    }
+
+    private async checkPassword(password: string, hash: string): Promise<boolean> {
+        try {
+            return await bcrypt.compare(password, hash);
+        } catch (error) {
+            logger.error("Error al verificar contraseña", "AuthenticationService", error);
+        }
+        return false;
+    }
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,7 @@ import { SnmpPollingService } from "./snmp-polling.service";
 import { SnmpTrapListenerService } from "./snmp-trap-listener.service";
 import { env } from "../config/env";
 import { UsersService } from "./users.service";
+import { AuthenticationService } from "./authentication.service";
 
 export * from "./snmp-polling.service";
 export * from "./devices.service";
@@ -14,6 +15,7 @@ const oidRecordsService = new OidRecordsService();
 const snmpPollingService = new SnmpPollingService(oidRecordsService);
 const devicesService = new DevicesService(snmpPollingService);
 const snmpTrapListenerService = new SnmpTrapListenerService(env.snmp.port);
+const authenticationService = new AuthenticationService();
 const usersService = new UsersService();
 
 process.on("SIGINT", () => {
