42f1a3c0a1663c7057732b084faa6104fe1fe86a
Álvaro Vaquero Giménez <alvaro.vaquero.tel@gmail.com>
2025-12-04 18:34:38 +0100

    Agregamos endpoint para enviar consultas de historicos

--- src/services/oid-records-db.service.ts
+++ src/services/oid-records-db.service.ts
@@ -15,4 +15,39 @@ export class OidRecordsDBService {
             logger.error(`Failed to insert record (dev: ${record.deviceId}) (oid: ${record.oid}):`, "OidRecordsDBService", err);
         }
     }
+
+    public static async findRecords(deviceId: number, oid: string, start: Date, end: Date) : Promise<OidRecord[]>
+    {
+        try {
+            const query = `
+                SELECT device_id, oid, value, error, type, date
+                FROM records
+                WHERE device_id = $1
+                AND oid = $2
+                AND date >= $3
+                AND date <= $4
+                ORDER BY date;
+            `;
+    
+            const { rows } = await pool.query(query, [deviceId, oid, start, end]);
+            const records: OidRecord[] = [];
+            for (const row of rows) {
+                const record: OidRecord = {
+                    deviceId: row.device_id,
+                    oid: row.oid,
+                    value: row.value,
+                    error: row.error,
+                    type: row.type,
+                    date: row.date
+                };
+                records.push(record);
+            }
+    
+            return records;
+        } catch (err) {
+            logger.error("Faile to find records", "OidRecordsDBService", err);
+        }
+
+        return [];
+    }
 }
